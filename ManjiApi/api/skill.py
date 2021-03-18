8274848f4342d28246356cab6b37cf574ada7790
ombe <h3236516@gmail.com>
2021-03-18 19:29:03 +0900

    rename

--- ManjiApi/api/skill.py
+++ ManjiApi/api/skill.py
@@ -1,21 +1,21 @@
 from sanic import Blueprint
 from sanic.response import json
 
-from ManjiApi.utils.skill_data_requester import YoshiRequester
+from ManjiApi.utils.skill_data import SkillData
 
-requester = YoshiRequester()
+skill_data = SkillData()
 skill = Blueprint(__name__, url_prefix="/skill")
 
 
 @skill.get("/all")
 async def all_skills(request):
-    resp = await requester.get_all_skills()
+    resp = await skill_data.get_all_skills()
     return json(resp, 200)
 
 
 @skill.get("/<num:int>")
 async def get_skill(request, num):
-    resp = await requester.get_skill_by_num(num)
+    resp = await skill_data.get_skill_by_num(num)
     if resp:
         return json({"info": resp}, 200)
     else:
--- ManjiApi/utils/skill_data.py
+++ ManjiApi/utils/skill_data.py
@@ -1,21 +1,21 @@
 import aiohttp
 
 
-class YoshiRequester:
+class SkillData:
     def __init__(self):
         self.data_url = "https://raw.githubusercontent.com/ombe1229/yoshimitsu_frame_data/master/yoshimitsu_ko.json"
 
     @staticmethod
-    async def request(method, url, **kwargs):
+    async def request(url, **kwargs):
         async with aiohttp.ClientSession() as cs:
-            async with cs.request(method, url, **kwargs) as r:
+            async with cs.get(url, **kwargs) as r:
                 if r.status != 200:
                     return None
                 response = await r.json(content_type=None)
                 return response
 
     async def get_all_skills(self):
-        resp = await self.request("GET", self.data_url)
+        resp = await self.request(self.data_url)
         return resp
 
     async def get_skill_by_num(self, num: int):
