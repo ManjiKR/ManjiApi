c88bb3d22f14ba7c2bd4f72d50d3b88d519a3735
ombe <h3236516@gmail.com>
2021-03-18 23:42:53 +0900

    idk

--- ManjiApi/utils/skill_data.py
+++ ManjiApi/utils/skill_data.py
@@ -6,16 +6,16 @@ class SkillData:
         self.data_url = "https://raw.githubusercontent.com/ombe1229/yoshimitsu_frame_data/master/yoshimitsu_ko.json"
 
     @staticmethod
-    async def request(url, **kwargs):
+    async def get(url):
         async with aiohttp.ClientSession() as session:
-            async with session.get(url, **kwargs) as r:
+            async with session.get(url) as r:
                 if r.status != 200:
                     return None
                 response = await r.json(content_type=None)
                 return response
 
     async def get_all_skills(self):
-        resp = await self.request(self.data_url)
+        resp = await self.get(self.data_url)
         return resp
 
     async def get_skill_by_num(self, num: int):
