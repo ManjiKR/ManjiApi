305b00746dd0e77b872b714a66a29b5bca11c479
ombe <h3236516@gmail.com>
2021-03-19 22:51:36 +0900

    add status

--- ManjiApi/api/skill.py
+++ ManjiApi/api/skill.py
@@ -17,6 +17,6 @@ async def all_skills(request):
 async def get_skill(request, num):
     resp = await skill_data.get_skill_by_num(num)
     if resp:
-        return json({"info": resp}, 200)
+        return json({"status": 200, "info": resp}, 200)
     else:
         return json({"status": 400, "message": "number is too big (max is 411)"}, 400)
