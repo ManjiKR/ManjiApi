2a7eebb09798918139b15554702388dc5afbf4f7
ombe <h3236516@gmail.com>
2021-03-22 10:41:59 +0900

    remove log

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
 from sanic import Sanic
 from sanic_openapi import swagger_blueprint
-from rich.logging import RichHandler
-import logging
 
 from ManjiApi.api.skill import skill
 from ManjiApi.api.gallery import gallery
 
-
-accessHandler = RichHandler(rich_tracebacks=True)
-accessHandler.setFormatter(
-    logging.Formatter("%(name)s :\t%(request)s %(message)s %(status)d %(byte)d")
-)
-
-genericHandler = RichHandler(rich_tracebacks=True)
-genericHandler.setFormatter(logging.Formatter("%(name)s :\t%(message)s"))
-
-logging.getLogger("sanic").setLevel(logging.INFO)
-
-logging.getLogger("sanic.root").addHandler(genericHandler)
-
-errorLogger = logging.getLogger("sanic.error")
-errorLogger.propagate = True
-errorLogger.addHandler(genericHandler)
-
-accessLogger = logging.getLogger("sanic.access")
-accessLogger.propagate = True
-accessLogger.addHandler(accessHandler)
 
 app = Sanic(__name__, configure_logging=False)
 app.blueprint(skill)
