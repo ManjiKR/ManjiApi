297eddbdb85b83aa0615b4b3c01d966846c39e5a
ombe <h3236516@gmail.com>
2021-03-25 12:00:11 +0900

    0.1.3

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ app.blueprint(gallery)
 app.blueprint(swagger_blueprint)
 
 app.config.FALLBACK_ERROR_FORMAT = "json"
-app.config["API_VERSION"] = "0.1.2"
+app.config["API_VERSION"] = "0.1.3"
 app.config["API_TITLE"] = "Manji Api"
-app.config["API_LISENCE_NAME"] = "Apache 2.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
