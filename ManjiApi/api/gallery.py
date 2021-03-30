9d1bf2e82553ed0b388e7a974560e648c1562409
ombe <h3236516@gmail.com>
2021-03-30 23:14:43 +0900

    remove doc

--- ManjiApi/api/gallery.py
+++ ManjiApi/api/gallery.py
@@ -1,6 +1,5 @@
 from sanic import Blueprint
 from sanic.response import json
-from sanic_openapi import doc
 
 from ManjiApi.utils.yoshigall import YoshiGall
 
@@ -9,20 +8,17 @@ gallery = Blueprint(__name__, url_prefix="/gallery")
 
 
 @gallery.get("/view/<no:int>")
-@doc.description("번호에 해당하는 요마갤 글을 불러옵니다.")
 async def get_post_info(request, no):
     resp = await yoshi_gall.post_view(no)
     return json(resp)
 
 
 @gallery.get("/tt/lists/<page:int>")
-@doc.description("요마갤 하루팁 말머리 리스트를 불러옵니다.")
 async def get_lists(request, page):
     resp = await yoshi_gall.tt_list(page)
     return json(resp)
 
 
 @gallery.get("/search")
-@doc.description("asdf")
 async def get_search(request):
     print(request.args)
--- ManjiApi/api/skill.py
+++ ManjiApi/api/skill.py
@@ -1,6 +1,5 @@
 from sanic import Blueprint
 from sanic.response import json
-from sanic_openapi import doc
 
 from ManjiApi.utils.skill_data import SkillData
 
@@ -9,14 +8,12 @@ skill = Blueprint(__name__, url_prefix="/skill")
 
 
 @skill.get("/all")
-@doc.description("모든 스킬 정보를 불러옵니다.")
 async def all_skills(request):
     resp = await skill_data.get_all_skills()
     return json(resp, 200)
 
 
 @skill.get("/<num:int>")
-@doc.description("번호에 해당하는 스킬 정보를 불러옵니다.")
 async def get_skill(request, num):
     resp = await skill_data.get_skill_by_num(num)
     if resp:
