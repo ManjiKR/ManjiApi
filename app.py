a34cd622b2ba41e87584f83df8bb7fbf98c7b21b
ombe <h3236516@gmail.com>
2021-04-28 18:19:51 +0900

    remove configs

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app = Sanic(__name__, configure_logging=False)
 app.blueprint(bp_group)
 
 app.config.FALLBACK_ERROR_FORMAT = "json"
-app.config["API_VERSION"] = "0.3.5"
-app.config["API_TITLE"] = "Manji Api"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
