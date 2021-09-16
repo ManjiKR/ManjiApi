90ee883c9f155d297a1198d049e7046cf2d2f6c9
YongChan Cho <h3236516@gmail.com>
2021-09-16 10:50:03 +0900

    feat(yoshigall): add post

---
+++ ManjiApi/api/yoshigall/post.py
@@ -0,0 +1,17 @@
+from sanic.views import HTTPMethodView
+from ManjiApi.sanic import ManjiApiRequest
+from sanic.blueprints import Blueprint
+from sanic.response import HTTPResponse, json
+
+
+yoshigall_post = Blueprint("yoshigall_post", url_prefix="/post")
+
+
+class YoshiGallPostView(HTTPMethodView):
+    async def get(self, request: ManjiApiRequest, post_id: int) -> HTTPResponse:
+        if post_info := await request.app.ctx.yoshigall_request.get_post(post_id):
+            return json({"status": 200, **post_info})
+        return json({"h": 0})
+
+
+yoshigall_post.add_route(YoshiGallPostView.as_view(), "/<post_id:int>")
