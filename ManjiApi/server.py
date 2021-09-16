cd77bbc11e4d09a9ef4a508eeedd165b0fa583d1
YongChan Cho <h3236516@gmail.com>
2021-09-16 10:15:04 +0900

    feat(server): framedata_request, typing

--- ManjiApi/server.py
+++ ManjiApi/server.py
@@ -1,20 +1,24 @@
+from ManjiApi.sanic import ManjiApi
+from ManjiApi.request.frame_data import FrameDataRequest
 from ManjiApi.request.yoshimitsu_gallery import YoshiGallRequest
 from aiohttp.client import ClientSession
 from ManjiApi.request.base import BaseRequest
 from asyncio.events import AbstractEventLoop
-from sanic import Sanic
+from sanic.app import Sanic
 
 
 manjiapi = Sanic("ManjiApi")
 
 
 @manjiapi.main_process_start
-async def start(manjiapi, loop: AbstractEventLoop) -> None:
+async def start(manjiapi: ManjiApi, loop: AbstractEventLoop) -> None:
     manjiapi.ctx.base_request = BaseRequest(ClientSession())
     manjiapi.ctx.yoshigall_request = await YoshiGallRequest.setup()
+    manjiapi.ctx.framedata_request = await FrameDataRequest.setup()
 
 
 @manjiapi.main_process_stop
-async def stop(manjiapi, loop: AbstractEventLoop) -> None:
+async def stop(manjiapi: ManjiApi, loop: AbstractEventLoop) -> None:
     await manjiapi.ctx.base_request.close()
     await manjiapi.ctx.yoshigall_request.close()
+    await manjiapi.ctx.framedata_request.close()
