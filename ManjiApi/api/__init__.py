65a38b9315ddec6b08e10cd8f79c4c50cff41ff5
YongChan Cho <h3236516@gmail.com>
2021-09-19 06:54:11 +0900

    feat(api): add skill_endpoint

--- ManjiApi/api/__init__.py
+++ ManjiApi/api/__init__.py
@@ -1,6 +1,6 @@
 from sanic.blueprints import Blueprint
 
 from ManjiApi.api.yoshigall import yoshigall_endpoint
+from ManjiApi.api.skill import skill_endpoint
 
-
-api_endpoint = Blueprint.group(yoshigall_endpoint, url_prefix="/api")
+api_endpoint = Blueprint.group(yoshigall_endpoint, skill_endpoint, url_prefix="/api")
