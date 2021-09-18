a6d0bb29548e8260c15586b9deae82ae219c5010
YongChan Cho <h3236516@gmail.com>
2021-09-19 06:54:01 +0900

    style(skill): rename to skill_endpoint

--- ManjiApi/api/skill/__init__.py
+++ ManjiApi/api/skill/__init__.py
@@ -3,4 +3,4 @@ from sanic.blueprints import Blueprint
 from ManjiApi.api.skill.all_skills import skill_all_skills
 from ManjiApi.api.skill.skill import skill_skill
 
-framedata_endpoint = Blueprint.group(skill_all_skills, skill_skill, url_prefix="/skill")
+skill_endpoint = Blueprint.group(skill_all_skills, skill_skill, url_prefix="/skill")
