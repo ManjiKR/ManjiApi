5ececfc0178fc2c8f69ff55cba21d9bdc160db81
YongChan Cho <h3236516@gmail.com>
2021-09-19 06:52:10 +0900

    feat(skill): add skill

---
+++ ManjiApi/api/skill/skill.py
@@ -0,0 +1,17 @@
+from sanic.views import HTTPMethodView
+from ManjiApi.sanic import ManjiApiRequest
+from sanic.blueprints import Blueprint
+from sanic.response import HTTPResponse, json
+
+
+skill_skill = Blueprint("skill_skill", url_prefix="/skill")
+
+
+class SkillSkillView(HTTPMethodView):
+    async def get(self, request: ManjiApiRequest, num: int) -> HTTPResponse:
+        if skill_info := await request.app.ctx.framedata_request.get_skill_by_num(num):
+            return json({"status": 200, **skill_info})
+        return json({"h": 0})
+
+
+skill_skill.add_route(SkillSkillView.as_view(), "/<num:int>")
