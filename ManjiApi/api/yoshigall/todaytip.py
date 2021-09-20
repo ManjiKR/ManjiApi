c6f8ce0d1744d906613fece9d35475ba2fd55282
YongChan Cho <h3236516@gmail.com>
2021-09-20 23:39:49 +0900

    feat(swagger): swagger

--- ManjiApi/api/yoshigall/todaytip.py
+++ ManjiApi/api/yoshigall/todaytip.py
@@ -2,12 +2,15 @@ from sanic.response import HTTPResponse, json
 from ManjiApi.sanic import ManjiApiRequest
 from sanic.blueprints import Blueprint
 from sanic.views import HTTPMethodView
+from sanic_openapi.openapi3.openapi import summary, tag
 
 
 yoshigall_todaytip = Blueprint("yoshigall_todaytip", url_prefix="/todaytip")
 
 
 class YoshiGallTodayTipView(HTTPMethodView):
+    @summary("Get todaytip list")
+    @tag("yoshigall")
     async def get(self, request: ManjiApiRequest, page: int) -> HTTPResponse:
         if todaytip := await request.app.ctx.yoshigall_request.get_todaytip(page):
             return json({"status": 200, **todaytip})
--- ManjiApi/server.py
+++ ManjiApi/server.py
@@ -1,5 +1,6 @@
 from ManjiApi.response import Response
 from ManjiApi.sanic import ManjiApi
+from ManjiApi import __version__
 
 from ManjiApi.request.base import BaseRequest
 from ManjiApi.request.frame_data import FrameDataRequest
@@ -10,15 +11,21 @@ from ManjiApi.api import api_endpoint
 from aiohttp.client import ClientSession
 from asyncio.events import AbstractEventLoop
 from sanic.app import Sanic
+from sanic_openapi import openapi3_blueprint
 
 
 manjiapi = Sanic("ManjiApi")
 manjiapi.blueprint(api_endpoint)
+manjiapi.blueprint(openapi3_blueprint)
 
 
 @manjiapi.main_process_start
 async def start(manjiapi: ManjiApi, loop: AbstractEventLoop) -> None:
     manjiapi.config.FALLBACK_ERROR_FORMAT = "json"
+    manjiapi.config.API_VERSION = __version__
+    manjiapi.config.API_TITLE = "ManjiApi"
+    manjiapi.config.API_DESCRIPTION = "Tekken 7 yoshimitsu api"
+    manjiapi.config.API_LICENSE_NAME = "Apache 2.0"
     manjiapi.ctx.base_request = BaseRequest(ClientSession())
     manjiapi.ctx.yoshigall_request = await YoshiGallRequest.setup()
     manjiapi.ctx.framedata_request = await FrameDataRequest.setup()
